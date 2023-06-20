18c3adfe6346e64212458f7db9590051eca9620d
athenamw <wallisathena@gmail.com>
2023-06-20 13:03:20 -0700

    created basic framework, added apis, created files

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+const weatherApi = 'http://api.openweathermap.org/data/2.5/forecast?lat=38.6446&lon=121.2722&appid=3114f25663213d6ad83469ab1d2aa40f';
+
+const geoApi = 'http://api.openweathermap.org/data/2.5/forecast?q=Sacramento&appid=3114f25663213d6ad83469ab1d2aa40f';
